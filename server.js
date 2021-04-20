fb7d1f03497faa8715269649e2b919c74edfc09a
Dodooatif <dodooatif@gmail.com>
2021-04-20 15:29:53 +0000

    added cors

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const morgan = require("morgan")
 const connectDB = require("./config/connectDB")
 const customersRoute = require("./routes/customersRoute")
 const usersRoute = require("./routes/usersRoute")
+const cors = require("cors")
 
 dotenv.config()
 
@@ -15,6 +16,7 @@ connectDB();
 //middlewares
 app.use(express.json());
 app.use(morgan("dev"));
+app.use(cors());
 
 //routes
 app.use("/api/v2/customers", customersRoute);
